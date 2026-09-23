bcd15827ca800bbf3d1d432bda75305cdd6239e3
Erling Jahr <erling.jahr@novari.no>
2026-09-23 13:57:57 +0200

    FKS-1679 added consumer for all source topics. No logic - just logging

--- src/main/kotlin/no/novari/fintkontrolluserservice/kafka/KafkaConsumer.kt
+++ src/main/kotlin/no/novari/fintkontrolluserservice/kafka/KafkaConsumer.kt
@@ -1,10 +1,16 @@
 package no.novari.fintkontrolluserservice.kafka
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import no.novari.cache.FintCache
 import no.novari.fint.model.resource.administrasjon.organisasjon.OrganisasjonselementResource
+import no.novari.fint.model.resource.administrasjon.personal.ArbeidsforholdResource
 import no.novari.fint.model.resource.administrasjon.personal.PersonalressursResource
 import no.novari.fint.model.resource.felles.PersonResource
+import no.novari.fint.model.resource.utdanning.elev.ElevResource
+import no.novari.fint.model.resource.utdanning.elev.ElevforholdResource
+import no.novari.fint.model.resource.utdanning.elev.SkoleressursResource
+import no.novari.fint.model.resource.utdanning.utdanningsprogram.SkoleResource
+import no.novari.fintkontrolluserservice.entra.EntraUser
+import no.novari.fintkontrolluserservice.entra.EntraUserExternal
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,18 +20,27 @@ private val logger = LoggerFactory.getLogger(KafkaConsumer::class.java)
 @Configuration
 class KafkaConsumer(
     private val containerFactory: KafkaContainerFactory,
-    private val organisasjonselementCache: FintCache<String, OrganisasjonselementResource>,
-    private val personalressursCache: FintCache<String, PersonalressursResource>,
+    private val organisasjonselementResourceCache: FintCache<String, OrganisasjonselementResource>,
+    private val personalressursResourceCache: FintCache<String, PersonalressursResource>,
+    private val ansattPersonResourceCache: FintCache<String, PersonResource>,
+    private val arbeidsforholdResourceCache: FintCache<String, ArbeidsforholdResource>,
+    private val elevResourceCache: FintCache<String, ElevResource>,
+    private val elevPersonCache: FintCache<String, PersonResource>,
+    private val elevforholdCache: FintCache<String, ElevforholdResource>,
+    private val skoleResourceCache: FintCache<String, SkoleResource>,
+    private val ansattSkoleressursResourceCache: FintCache<String, SkoleressursResource>,
+    private val graphUserCache: FintCache<String, EntraUser>,
+    private val graphUserExternalCache: FintCache<String, EntraUserExternal>
 ) {
     @Bean
     fun organisasjonselementConsumer() =
         containerFactory.createContainer(
             topicName = "administrasjon-organisasjon-organisasjonselement",
             consumingClass = OrganisasjonselementResource::class,
-            cache = organisasjonselementCache,
+            cache = organisasjonselementResourceCache,
             handler = { key, value ->
                 logger.info("Consumed:: organisasjonselement ::  $key :: ${value.navn ?: "no navn"}")
-                // TODO: create orgunitHandler
+                // TODO: create handler
             },
         )
 
@@ -33,11 +48,111 @@ class KafkaConsumer(
     fun personalressursConsumer() =
         containerFactory.createContainer(
             topicName = "administrasjon-personal-personalressurs",
-            consumingClass = PersonResource::class,
-            cache = organisasjonselementCache,
+            consumingClass = PersonalressursResource::class,
+            cache = personalressursResourceCache,
             handler = { key, value ->
-                logger.info("Consumed:: organisasjonselement ::  $key :: ${value.navn ?: "no navn"}")
-                // TODO: create orgunitHandler
+                logger.info("Consumed:: personalressurs ::  $key :: ${value.ansattnummer ?: "no employeeNumber"}")
+                // TODO: create handler
             },
         )
+
+    @Bean
+    fun ansattPersonResourceConsumer() =
+        containerFactory.createContainer(
+            topicName = "administrasjon-personal-person",
+            consumingClass = PersonResource::class,
+            cache = ansattPersonResourceCache,
+            handler = { key, value ->
+                logger.info("Consumed:: ansatt person :: $key :: ${value.navn ?: "no name"}")
+            },
+        )
+
+    @Bean
+    fun arbeidsforholdResourceConsumer() =
+        containerFactory.createContainer(
+            topicName = "administrasjon-personal-arbeidsforhold",
+            consumingClass = ArbeidsforholdResource::class,
+            cache = arbeidsforholdResourceCache,
+            handler = { key, value ->
+                logger.info("Consumed:: arbeidsforhold :: $key :: ${value.systemId.identifikatorverdi ?: "no systemId"}")
+            },
+        )
+
+    @Bean
+    fun elevConsumer() =
+        containerFactory.createContainer(
+            topicName = "utdanning-elev-elev",
+            consumingClass = ElevResource::class,
+            cache = elevResourceCache,
+            handler = { key, value ->
+                logger.info("Consumed:: elev :: $key :: ${value.brukernavn.identifikatorverdi ?: "no brukernavn"}")
+            },
+        )
+
+    @Bean
+    fun elevPersonConsumer() =
+        containerFactory.createContainer(
+            topicName = "utdanning-elev-person",
+            consumingClass = PersonResource::class,
+            cache = elevPersonCache,
+            handler = { key, value ->
+                logger.info("Consumed:: elevperson :: $key :: ${value.navn ?: "no name"}")
+            },
+        )
+
+    @Bean
+    fun elevforholdConsumer() =
+        containerFactory.createContainer(
+            topicName = "utdanning-elev-elevforhold",
+            consumingClass = ElevforholdResource::class,
+            cache = elevforholdCache,
+            handler = { key, value ->
+                logger.info("Consumed:: elevforhold :: $key :: ${value.systemId.identifikatorverdi ?: "no systemId"}")
+            },
+        )
+
+    @Bean
+    fun skoleConsumer() =
+        containerFactory.createContainer(
+            topicName = "utdanning-utdanningsprogram-skole",
+            consumingClass = SkoleResource::class,
+            cache = skoleResourceCache,
+            handler = { key, value ->
+                logger.info("Consumed:: skole :: $key :: ${value.navn ?: "no name"}")
+            },
+        )
+
+    @Bean
+    fun ansattSkoleConsumer() =
+        containerFactory.createContainer(
+            topicName = "utdanning-elev-skoleressurs",
+            consumingClass = SkoleressursResource::class,
+            cache = ansattSkoleressursResourceCache,
+            handler = { key, value ->
+                logger.info("Consumed:: ansatt skole :: $key :: ${value.systemId.identifikatorverdi ?: "no systemId"}")
+            },
+        )
+
+    @Bean
+    fun entraUserConsumer() =
+        containerFactory.createContainer(
+            topicName = "graph-user",
+            consumingClass = EntraUser::class,
+            cache = graphUserCache,
+            handler = { key, value ->
+                logger.info("Consumed:: entra user :: $key :: ${value.userPrincipalName ?: "no userprincipalname"}")
+            },
+        )
+
+    @Bean
+    fun externalEntraUserConsumer() =
+        containerFactory.createContainer(
+            topicName = "graph-user-external",
+            consumingClass = EntraUserExternal::class,
+            cache = graphUserExternalCache,
+            handler = { key, value ->
+                logger.info("Consumed:: entra user external :: $key :: ${value.userPrincipalName ?: "no userprincipalname"}")
+            }
+        )
+
 }
